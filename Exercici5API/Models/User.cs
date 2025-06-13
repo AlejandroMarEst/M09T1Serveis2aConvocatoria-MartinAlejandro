d61fe4d8ed1e4badde7ae45f2e6b4c52baf6d799
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-06-13 03:16:01 +0200

    Filled user model

--- Exercici5API/Models/User.cs
+++ Exercici5API/Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace Exercici5API.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercici5API.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public string? CompanyName { get; set; }
+        public string? CEOName { get; set; }
+        public int? NumberOfAttendees { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+        public bool? IsVip { get; set; }
     }
 }
